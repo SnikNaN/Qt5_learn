2861aca527145b05bfa92568e6aec2bcc876ccfb
SnikNaN <you@example.com>
2023-03-06 12:31:33 +0300

    correct ch_07

--- chapter_07/main.cpp
+++ chapter_07/main.cpp
@@ -1,11 +1,5 @@
 // ======================================================================
-//  main.cpp
-// ======================================================================
-//                   This file is a part of the book 
-//             "Qt 5.10 Professional programming with C++"
-//                       http://qt-book.com
-// ======================================================================
-//  Copyright (c) 2017 by Max Schlee
+// LCD Clock with milliseconds as progress bar
 // ======================================================================
 
 #include "Headers/Clock.hpp"
