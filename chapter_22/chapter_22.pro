b13e857d26b63ab818e2101c252952a459b1ec37
SnikNaN <you@example.com>
2023-03-20 16:06:33 +0300

    chapter_22

---
+++ chapter_22/chapter_22.pro
@@ -0,0 +1,6 @@
+TEMPLATE     = app
+QT          += widgets
+SOURCES	     = main.cpp
+RESOURCES    = resource.qrc
+windows:TARGET = ../chapter_22
+
---
+++ chapter_22/main.cpp
@@ -0,0 +1,50 @@
+#include <QtWidgets>
+
+// ----------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);    
+    QLabel       lbl;
+    QMovie       mov(":/anim.mng");
+
+    lbl.setAttribute(Qt::WA_TranslucentBackground);
+    lbl.setWindowFlag(Qt::FramelessWindowHint);
+    lbl.setWindowFlag(Qt::WindowStaysOnTopHint);
+
+    mov.start();
+
+    lbl.setMovie(&mov);
+    lbl.resize(800, 800);
+
+    QPropertyAnimation anim1(&mov, "speed");
+    anim1.setStartValue(100);
+    anim1.setKeyValueAt(0.25f, 200);
+    anim1.setKeyValueAt(0.5f, 400);
+    anim1.setKeyValueAt(0.75f, 150);
+    anim1.setEndValue(100);
+    anim1.setDuration(12000);
+    anim1.setLoopCount(-1);
+
+    QPropertyAnimation anim2(&lbl, "geometry");
+    anim2.setStartValue(QRect(0, 0, 128, 128));
+    anim2.setKeyValueAt(0.25f, QRect(800, 200, 128, 128));
+    anim2.setKeyValueAt(0.5f, QRect(qApp->screens().at(0)->size().width() - 128,
+                                    qApp->screens().at(0)->size().width() - 128,
+                                    128, 128));
+    anim2.setKeyValueAt(0.75f, QRect(200, 500, 128, 128));
+    anim2.setEndValue(QRect(0, 0, 128, 128));
+    anim2.setDuration(15000);
+    anim2.setLoopCount(-1);
+    anim2.setEasingCurve(QEasingCurve::InOutBounce);
+
+    QParallelAnimationGroup agroup;
+    agroup.addAnimation(&anim1);
+    agroup.addAnimation(&anim2);
+
+    agroup.start();
+
+    lbl.show();
+
+    return app.exec();
+}
+
